4d9266244a0903c3082f35bf75c191e7fd6b8e7c
masyuko0222 <masyuko0222@gmail.com>
2022-11-20 05:28:40 +0900

    コメントの追記、変数名の一部修正を行いました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,13 +4,15 @@ def set_files_horizontally
   files_no_dot = Dir.entries('.').sort.grep_v(/^\./)
 
   # 最大列数を定義
-  rows = 3
+  max_rows = 3
 
-  # transposeに備えて配列の要素数を合わせる
-  lines = (files_no_dot.size % rows).zero? ? (files_no_dot.size / rows) : (files_no_dot.size / rows) + 1
+  # (例) files_no_dot (12), max_rows(3) => (4)(4)(4)の要素数を持った配列にslice
+  # (例) files_no_dot (13), max_rows(3) => (5)(5)(3)の要素数を持った配列にslice
+  lines = (files_no_dot.size % max_rows).zero? ? (files_no_dot.size / max_rows) : (files_no_dot.size / max_rows) + 1
   files_horizontally = files_no_dot.each_slice(lines).to_a
-  max_elements = files_horizontally.max_by(&:size).size
 
+  # 各配列の要素数が一致していない場合、nilを追加して要素数を全て揃える
+  max_elements = files_horizontally.max_by(&:size).size
   files_horizontally.each do |arr|
     arr << nil while arr.size < max_elements
   end
@@ -24,9 +26,9 @@ def ls_command
   end
 
   max_chars_filename = files_vertically.flatten.max_by(&:length).length
-
   files_vertically.each do |arr|
     arr.each do |file|
+      # どれだけ長いファイル名でも、綺麗に表示できるようにする
       print file.ljust(max_chars_filename + 1)
     end
     print "\n"
