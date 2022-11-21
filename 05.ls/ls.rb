f1e22fa263c0380a4b1375eece0b3d4058825e8b
masyuko0222 <masyuko0222@gmail.com>
2022-11-22 02:10:20 +0900

    メソッド内で定義されていた変数をトップレベルに移動しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-def set_files_horizontally
-  files_no_dot = Dir.entries('.').sort.grep_v(/^\./)
+# 表示するファイルと列数は、メソッドで処理を行う前に決められるので、トップレベルで定義。
+@files_no_dot = Dir.entries('.').sort.grep_v(/^\./)
+@columns = 3
 
-  # 最大列数を定義
-  max_rows = 3
-
-  # (例) files_no_dot (12), max_rows(3) => (4)(4)(4)の要素数を持った配列にslice
-  # (例) files_no_dot (13), max_rows(3) => (5)(5)(3)の要素数を持った配列にslice
-  lines = (files_no_dot.size % max_rows).zero? ? (files_no_dot.size / max_rows) : (files_no_dot.size / max_rows) + 1
-  files_horizontally = files_no_dot.each_slice(lines).to_a
+def save_files_horizontally
+  # transpose前は3行表示になるように分割(transpose後に3列表示になるために)
+  lines = (@files_no_dot.size % @columns).zero? ? (@files_no_dot.size / @columns) : (@files_no_dot.size / @columns) + 1
+  files_horizontally = @files_no_dot.each_slice(lines).to_a
 
   # 各配列の要素数が一致していない場合、nilを追加して要素数を全て揃える
   max_elements = files_horizontally.max_by(&:size).size
@@ -20,8 +18,8 @@ def set_files_horizontally
   files_horizontally
 end
 
-def ls_command
-  files_vertically = set_files_horizontally.transpose.each do |arr|
+def display_files_vertically
+  files_vertically = save_files_horizontally.transpose.each do |arr|
     arr.delete(nil)
   end
 
@@ -35,4 +33,4 @@ def ls_command
   end
 end
 
-ls_command
+display_files_vertically
