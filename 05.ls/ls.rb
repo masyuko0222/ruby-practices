d0aad3e41d929cb3a5ea96fa8ac1b3389271b51e
masyuko0222 <masyuko0222@gmail.com>
2022-11-23 15:36:53 +0900

    変数名の修正、およびメソッドの分割をおこないました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,32 +1,32 @@
 # frozen_string_literal: true
 
-# 表示する列数は仕様段階で決まるので、定数として定義しました。
-COLUMNS = 3
+COLUMN = 3
 
 def main
-  # mainメソッド実行時にnilが表示されないように、transpose後にnilの削除も一緒に行っています。
-  after_transposing_files = ready_files_to_transpose.transpose.each do |files|
-    files.delete(nil)
-  end
-
-  after_transposing_files.each do |files|
+  ready_files_to_transpose.each do |files|
     files.each do |file|
-      print file.ljust(@filname_max_char + 1)
+      print file.ljust(count_filname_char + 1) if file
     end
     print "\n"
   end
 end
 
+def load_files
+  Dir.entries('.').sort.grep_v(/^\./)
+end
+
+def count_filname_char
+  load_files.max_by(&:length).length
+end
+
 def ready_files_to_transpose
-  non_dot_files = Dir.entries('.').sort.grep_v(/^\./)
+  non_dot_files = load_files
 
-  # この変数を利用するのはmainメソッド内のため、インスタンス変数として定義しています。
-  @filname_max_char = non_dot_files.max_by(&:length).length
+  line_length = (non_dot_files.size.to_f / COLUMN).ceil
 
-  line = (non_dot_files.size.to_f / COLUMNS).ceil
+  non_dot_files << nil until (non_dot_files.size % line_length).zero?
 
-  non_dot_files << nil until (non_dot_files.size % line).zero?
-  non_dot_files.each_slice(line).to_a
+  non_dot_files.each_slice(line_length).to_a.transpose
 end
 
 main
