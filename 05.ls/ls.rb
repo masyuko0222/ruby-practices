81fe1ae1afa906205a9d5e2ed30d4ee8c9d76254
masyuko0222 <masyuko0222@gmail.com>
2022-11-25 00:25:06 +0900

    指摘部分の修正を行いました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-COLUMN = 3
+COLUMN_LENGTH = 3
 
 def main
-  ready_files_to_display.each do |files|
+  max_filename_char = count_filename_char
+
+  prepare_files_to_display.each do |files|
     files.each do |file|
-      print file.ljust(count_filname_char + 1) if file
+      print file.ljust(max_filename_char + 1) if file
     end
     print "\n"
   end
@@ -15,14 +17,15 @@ def load_files
   Dir.entries('.').sort.grep_v(/^\./)
 end
 
-def count_filname_char
-  load_files.max_by(&:length).length
+def count_filename_char
+  non_dot_files = load_files
+  non_dot_files.max_by(&:length).length
 end
 
-def ready_files_to_display
+def prepare_files_to_display
   non_dot_files = load_files
 
-  line_length = (non_dot_files.size.to_f / COLUMN).ceil
+  line_length = (non_dot_files.size.to_f / COLUMN_LENGTH).ceil
 
   non_dot_files << nil until (non_dot_files.size % line_length).zero?
 
