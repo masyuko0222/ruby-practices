813b475407e45ba77fe5fc23da200cd697321c10
masyuko0222 <masyuko0222@gmail.com>
2022-11-30 00:07:16 +0900

    指摘点の修正を行いました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,9 +6,9 @@ def main
   files = load_files
 
   max_filename_length = files.max_by(&:length).length
-  transposed_files = build_file_tables(files)
+  file_table = build_file_table(files)
 
-  transposed_files.each do |files|
+  file_table.each do |files|
     files.each do |file|
       print file.ljust(max_filename_length + 1) if file
     end
@@ -20,11 +20,11 @@ def load_files
   Dir.entries('.').sort.grep_v(/^\./)
 end
 
-def build_file_tables(files)
+def build_file_table(files)
   line_length = (files.size.to_f / COLUMN_LENGTH).ceil
 
   adding_nil_count = line_length * COLUMN_LENGTH - files.size
-  adding_nil_count.times { files += [nil] }
+  files += Array.new(adding_nil_count)
 
   files.each_slice(line_length).to_a.transpose
 end
