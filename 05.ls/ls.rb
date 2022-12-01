d9bb2f603f5043a476ad76c92c2bd47c4e95ef70
masyuko0222 <masyuko0222@gmail.com>
2022-12-01 19:31:36 +0900

    lsコマンド②の課題を作成しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 COLUMN_LENGTH = 3
 
 def main
-  files = load_files
+  loaded_files = load_files(add_option)
 
-  max_filename_length = files.max_by(&:length).length
-  file_table = build_file_table(files)
+  max_filename_length = loaded_files.max_by(&:length).length
+  file_table = build_file_table(loaded_files)
 
   file_table.each do |files|
     files.each do |file|
@@ -16,8 +18,23 @@ def main
   end
 end
 
-def load_files
-  Dir.entries('.').sort.grep_v(/^\./)
+def add_option
+  opt = OptionParser.new
+
+  params = {}
+
+  opt.on('-a', 'do not ignore entries starting with .') { |v| params[:a] = v }
+  opt.parse!(ARGV)
+
+  params
+end
+
+def load_files(options)
+  if options[:a]
+    Dir.entries('.').sort
+  else
+    Dir.entries('.').sort.grep_v(/^\./)
+  end
 end
 
 def build_file_table(files)
