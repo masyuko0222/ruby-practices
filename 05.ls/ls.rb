41d8603aa06eb91984e9e63277c07940ef7c79f2
masyuko0222 <masyuko0222@gmail.com>
2022-12-11 18:52:07 +0900

    ハッシュの格納する値に誤字があったので一部修正しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,7 +4,7 @@ require 'optparse'
 require 'etc'
 
 COLUMN_LENGTH = 3
-FILE_TYPE_TO_CHAR = { '01' => 'p', '02' => 'c', '04' => 'd', '6' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+FILE_TYPE_TO_CHAR = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
 FILE_PERMISSION_TO_CHAR = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
 
 def main
