1a9c7f907192759ea58f8b1abf812dabd1f1aa29
masyuko0222 <masyuko0222@gmail.com>
2023-07-03 19:42:44 +0900

    リファクタリング: initializeではFrameオブジェクトの配列をインスタンス変数に保持するように

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -1,19 +1,38 @@
 # frozen_string_literal:true
 
 require_relative 'frame'
+require 'debug'
 
 class Game
   def initialize(marks)
-    @marks = marks
+    @frames = store_frames(marks)
+  end
+
+  def store_frames(marks)
+    frames = []
+    temporary_marks = []
+
+    marks.each_with_index do |mark, index|
+      temporary_marks << mark
+
+      if frames.size < 9
+        if temporary_marks.size >= 2 || mark == 'X'
+          frames << Frame.new(*temporary_marks)
+          temporary_marks.clear
+        end
+      elsif index == marks.count - 1
+        frames << Frame.new(*temporary_marks)
+      end
+    end
+
+    frames
   end
 
   def score
-    frames = store_frames
-
     score = 0
 
     10.times do |frame_number|
-      frame, next_frame, after_next_frame = frames.slice(frame_number, 3)
+      frame, next_frame, after_next_frame = @frames.slice(frame_number, 3)
       next_frame ||= nil
       after_next_frame ||= nil
 
@@ -32,25 +51,5 @@ class Game
     end
 
     score
-  end
-
-  def store_frames
-    frames = []
-    temporary_marks = []
-
-    @marks.each_with_index do |mark, index|
-      temporary_marks << mark
-
-      if frames.size < 9
-        if temporary_marks.size >= 2 || mark == 'X'
-          frames << Frame.new(*temporary_marks)
-          temporary_marks.clear
-        end
-      elsif index == @marks.count - 1
-        frames << Frame.new(*temporary_marks)
-      end
-    end
-
-    frames
   end
 end
