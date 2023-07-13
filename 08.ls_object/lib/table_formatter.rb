00d0fa786124138cc83733670b12ada875ca5ae4
masyuko0222 <masyuko0222@gmail.com>
2023-07-13 19:27:50 +0900

    修正: debugをrequireしていたのを削除

---
+++ 08.ls_object/lib/table_formatter.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require_relative './files'
+require_relative './files_table'
+
+class TableFormatter
+  def initialize(params)
+    @params = params
+    @files = Files.new(params).load
+    @files_table = FilesTable.new(params).build
+  end
+
+  def format
+    @params[:long_format] ? long_format : short_format
+  end
+
+  private
+
+  def short_format
+    max_file_name_length = calculate_max_file_name_length
+
+    @files_table.map do |files_in_line|
+      files_in_line.map do |file|
+        file.ljust(max_file_name_length + 1)
+      end.join.rstrip
+    end
+  end
+
+  def calculate_max_file_name_length
+    @files.max_by(&:length).length
+  end
+
+  def long_format
+    total_block = "total #{calculate_total_block}"
+    length_of_max_file_size = calculate_max_file_size_length
+
+    formatted_table =
+      @files_table.map do |information|
+        formatted_string = information[:file_type] + information[:file_permission] + 
+        " #{information[:hardlink_count].to_s}" +
+        " #{information[:user_name]}" +
+        " #{information[:group_name]}" +
+        " #{information[:file_size].to_s.rjust(length_of_max_file_size)}" +
+        " #{information[:time_stamp]}" +
+        " #{information[:file_name]}"
+
+        formatted_string += " -> #{File.readlink(information[:file_name])}" if information[:symlink?]
+
+        formatted_string
+      end
+
+    [total_block, *formatted_table]
+  end
+
+  def calculate_total_block
+    @files_table.sum { |information| information[:block_size] / 2 }
+  end
+
+  def calculate_max_file_size_length
+    @files_table.map{ |information| information[:file_size] }.max.to_s.length
+  end
+end
---
+++ 08.ls_object/test/table_formatter_test.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal:true
+
+require 'minitest/autorun'
+require_relative '../lib/table_formatter'
+
+class TableFormatterTest < Minitest::Test
+  def test_short_format_without_options
+    params = { all_files: false, reverse_in_sort: false, long_format: false }
+    table_formatter = TableFormatter.new(params)
+
+    expected_format = [
+      'THIS_IS_BIG_FILE                                    loooooooooooooooooooooooooooooooooooooooongtext.txt shortfolder',
+      'THIS_IS_BIG_FOLDER                                  nine                                                six',
+      'aggressive_link                                     old.rb                                              ten',
+      'eight                                               one                                                 this_is_small_file',
+      'five                                                passive_link                                        this_is_small_folder',
+      'four                                                seven                                               three',
+      'loooooooooooooooooooooooooooooooooooooooongfolder   short.txt                                           two',
+    ]
+
+    assert_equal expected_format, table_formatter.format
+  end
+
+  def test_long_format_without_all_and_reverse
+    params = { all_files: false, reverse_in_sort: false, long_format: true}
+
+    table_formatter = TableFormatter.new(params)
+
+    expected_format = [
+      'total 20',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 THIS_IS_BIG_FILE',
+      'drwxr-xr-x 2 masyuko0222 masyuko0222 4096 Jul 12 14:06 THIS_IS_BIG_FOLDER',
+      'lrwxrwxrwx 1 masyuko0222 masyuko0222   12 Jul 12 14:07 aggressive_link -> passive_link',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 eight',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 five',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 four',
+      'drwxr-xr-x 2 masyuko0222 masyuko0222 4096 Jul 12 14:06 loooooooooooooooooooooooooooooooooooooooongfolder',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 loooooooooooooooooooooooooooooooooooooooongtext.txt',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 nine',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222 3116 Jul 13 17:42 old.rb',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 one',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:06 passive_link',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 seven',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 short.txt',
+      'drwxr-xr-x 2 masyuko0222 masyuko0222 4096 Jul 12 14:06 shortfolder',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 six',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 ten',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 this_is_small_file',
+      'drwxr-xr-x 2 masyuko0222 masyuko0222 4096 Jul 12 14:06 this_is_small_folder',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 three',
+      '-rw-r--r-- 1 masyuko0222 masyuko0222    0 Jul 12 14:05 two'
+    ]
+
+    assert_equal expected_format, table_formatter.format
+  end
+end
