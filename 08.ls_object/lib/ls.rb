8d910a06f7d3d26930fbbdeefc83badaba62e4f6
masyuko0222 <masyuko0222@gmail.com>
2023-07-13 19:28:07 +0900

    新規: メインのコードを作成

---
+++ 08.ls_object/lib/ls.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative './table_formatter'
+require 'optparse'
+
+def load_options
+  opt = OptionParser.new
+
+  options = { all_files: false, reverse_in_sort: false, long_format: false }
+
+  opt.on('-a', 'do not ignore entries starting with .') { |v| options[:all_files] = v }
+  opt.on('-r', 'reverse order while sorting') { |v| options[:reverse_in_sort] = v }
+  opt.on('-l', 'use a long listing format') { |v| options[:long_format] = v }
+  opt.parse!(ARGV)
+
+  options
+end
+
+params = load_options
+
+puts TableFormatter.new(params).format
