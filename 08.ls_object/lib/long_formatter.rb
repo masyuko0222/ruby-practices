3ac34117da6ccf33d6132f074c2d309b08684878
masyuko0222 <masyuko0222@gmail.com>
2023-07-23 15:18:27 +0900

    FileStateを配列に格納する責務をlong formatterに

--- 08.ls_object/lib/long_formatter.rb
+++ 08.ls_object/lib/long_formatter.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 require_relative './file_state'
-require 'debug'
 
 class LongFormatter
-  def initialize(file_names, states_of_files)
+  def initialize(file_names)
     @file_names = file_names
-    @states_of_files = states_of_files
   end
 
   def format
-    total_block = @states_of_files.map { |state_of_file| state_of_file[:block_size] / 2 }.sum
+    states_of_files = load_states_of_files
+    total_block = states_of_files.map { |state_of_file| state_of_file[:block_size] / 2 }.sum
+    rjust_length = states_of_files.map { |state_of_file| state_of_file[:file_size] }.max.to_s.length
 
     formatted_file_names =
-      @states_of_files.map do |state_of_file|
+      states_of_files.map do |state_of_file|
         base_format =
           state_of_file[:file_type].to_s +
           state_of_file[:file_permission] +
           " #{state_of_file[:hardlink_count]}" \
           " #{state_of_file[:user_name]}" \
           " #{state_of_file[:group_name]}" \
-          " #{state_of_file[:file_size].to_s.rjust(rjust_length_of_file_size)}" \
+          " #{state_of_file[:file_size].to_s.rjust(rjust_length)}" \
           " #{state_of_file[:time_stamp]}" \
           " #{state_of_file[:file_name]}"
 
-        add_link_format(base_format, state_of_file) if state_of_file[:symlink?]
+        add_link(base_format, state_of_file) if state_of_file[:symlink?]
 
         base_format
       end
@@ -34,11 +34,11 @@ class LongFormatter
 
   private
 
-  def rjust_length_of_file_size
-    @states_of_files.map { |state_of_file| state_of_file[:file_size] }.max.to_s.length
+  def load_states_of_files
+    @file_names.map { |file_name| FileState.new(file_name).load_for_long_format }
   end
 
-  def add_link_format(base_format, state_of_file)
+  def add_link(base_format, state_of_file)
     base_format.concat(" -> #{state_of_file[:link_to]}")
   end
 end
