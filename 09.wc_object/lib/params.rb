5d88e7ddc98e9e62e63ecdea7c9f58d17ce9916d
masyuko0222 <masyuko0222@gmail.com>
2023-08-01 21:13:38 +0900

    optionsのkey命名をちょい変更

--- 09.wc_object/lib/params.rb
+++ 09.wc_object/lib/params.rb
@@ -7,14 +7,14 @@ class Params
     opt = OptionParser.new
 
     options = {}
-    opt.on('-l') {|v| options[:lines_count_opt] = v }
-    opt.on('-w') {|v| options[:words_count_opt] = v }
+    opt.on('-l') {|v| options[:lines_opt] = v }
+    opt.on('-w') {|v| options[:words_opt] = v }
     opt.on('-c') {|v| options[:byte_size_opt] = v }
 
     opt.parse!(argv)
 
     @options =
-      options.empty? ? { lines_count_opt: true, words_count_opt: true, byte_size_opt: true } : options
+      options.empty? ? { lines_opt: true, words_opt: true, byte_size_opt: true } : options
 
     @file_paths = argv
   end
