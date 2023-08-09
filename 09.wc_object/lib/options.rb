c38e0ec947f6b1bfa1dd988bc2aeb74b8e3db136
masyuko0222 <masyuko0222@gmail.com>
2023-08-09 11:41:09 +0900

    Optionsクラス生成

---
+++ 09.wc_object/lib/options.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require 'debug'
+
+class Options
+  def initialize(argv)
+    @argv = argv
+  end
+
+  def load
+    opt = OptionParser.new
+
+    tmp_options = { show_lines: false, show_words: false, show_byte_size: false }
+    opt.on('-l') {|v| tmp_options[:show_lines] = v }
+    opt.on('-w') {|v| tmp_options[:show_words] = v }
+    opt.on('-c') {|v| tmp_options[:show_byte_size] = v }
+    opt.parse!(@argv)
+
+    options = tmp_options.values.none? ? tmp_options.transform_values { |v| v = true } : tmp_options
+    options
+  end
+end
