c15f4e4bc28637726733fc1d3f6191eaa5c0b537
masyuko0222 <masyuko0222@gmail.com>
2023-08-12 18:54:01 +0900

    DRYに

--- 09.wc_object/lib/format.rb
+++ 09.wc_object/lib/format.rb
@@ -9,53 +9,51 @@ class Format
   end
 
   def format
-    each_text_counts =
-      @texts.map do |text|
-        count_list = []
-        count_list << text.lines if @show_lines
-        count_list << text.words if @show_words
-        count_list << text.bytesize if @show_bytesize
-        count_list
-      end
+    each_text_counts = store_all_counts(@texts)
+    text_counts_max_width = each_text_counts.flatten.max
 
-    total_counts =
-      @texts.map do |text|
-        count_list = []
-        count_list << text.lines if @show_lines
-        count_list << text.words if @show_words
-        count_list << text.bytesize if @show_bytesize
-      end.transpose.map(&:sum) if @texts.size >= 2
-
+    total_counts = calc_total(each_text_counts) if @texts.size >= 2
     total_counts_max_width = total_counts&.max || 0
-    width = [each_text_counts.flatten.max, total_counts_max_width].max.to_s.length
 
-    count_line =
+    width = [text_counts_max_width, total_counts_max_width].max.to_s.length
+
+    body_format =
       @texts.map do |text|
-        count_list = []
-        count_list << text.lines.to_s.rjust(width) if @show_lines
-        count_list << text.words.to_s.rjust(width) if @show_words
-        count_list << text.bytesize.to_s.rjust(width) if @show_bytesize
-        count_list << text.source_path
-        count_list.join(' ')
+        counts_format = store_counts(text).map { |count| rjust_width(count, width) }
+        line_format = counts_format << text.source_path
+        line_format.join(' ')
       end
 
     if @texts.size >= 2
-      total_line =
-        @texts.map do |text|
-          count_list = []
-          count_list << text.lines if @show_lines
-          count_list << text.words if @show_words
-          count_list << text.bytesize if @show_bytesize
-        end.transpose.map(&:sum).map { |total| total.to_s.rjust(width) }.join(' ')
-
-        total_line << ' total'
-        total_line
+      total_counts = total_counts
+      total_format = total_counts.map { |total| total.to_s.rjust(width) }.join(' ')
+      total_format << ' total'
+      total_format
     end
 
-    all_lines = total_line ? count_line.push(total_line) : count_line
+    all_lines = total_format ? body_format.push(total_format) : body_format
     all_lines
   end
 
   private
 
+  def calc_total(counts)
+    counts.transpose.map(&:sum)
+  end
+
+  def store_all_counts(texts)
+    texts.map { |text| store_counts(text) }
+  end
+
+  def store_counts(text)
+    count_list = []
+    count_list << text.lines if @show_lines
+    count_list << text.words if @show_words
+    count_list << text.bytesize if @show_bytesize
+    count_list
+  end
+
+  def rjust_width(content, width)
+    content.to_s.rjust(width)
+  end
 end
