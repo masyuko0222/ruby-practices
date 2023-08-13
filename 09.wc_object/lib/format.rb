4e93c22ca42e52a24e39ca2ee662023748f2aac6
masyuko0222 <masyuko0222@gmail.com>
2023-08-13 20:34:46 +0900

    実装完了

--- 09.wc_object/lib/format.rb
+++ 09.wc_object/lib/format.rb
@@ -11,36 +11,33 @@ class Format
   def render
     count_width = calc_max_count_width(@texts)
 
-    if @texts.size == 1
-      format_lines_of_counts(@texts, count_width)
+    if single_text?
+      format(@texts, count_width)
     else
-      format_lines_with_total(@texts, count_width)
+      format_with_total(@texts, count_width)
     end
   end
 
   private
 
+  # methods used in `render` method
+  def single_text?
+    @texts.size == 1
+  end
+
   def calc_max_count_width(texts)
     counts_list = texts.map { |text| store_counts(text) }
 
-    if texts.size == 1
+    if single_text?
       counts_list.flatten.max.to_s.length
     else
       # If you will output total line, total value is max number.
       # So max width is based on total(sum) values.
-      counts_list.transpose.map(&:sum).max.to_s.length
+      calc_total_of_each_counts(counts_list).max.to_s.length
     end
   end
 
-  def store_counts(text)
-    counts = []
-    counts << text.lines if @show_lines
-    counts << text.words if @show_words
-    counts << text.bytesize if @show_bytesize
-    counts
-  end
-
-  def format_lines_of_counts(texts, width)
+  def format(texts, width)
     texts.map do |text|
       counts = store_counts(text)
       formatted_counts = rjust_counts_width(counts, width)
@@ -49,16 +46,32 @@ class Format
     end
   end
 
-  def format_lines_with_total(texts, width)
-    counts_format = format_lines_of_counts(texts, width)
+  def format_with_total(texts, width)
     counts_list = texts.map { |text| store_counts(text) }
+    total_line_format = format_line_of_total(counts_list, width)
 
-    total_counts = counts_list.transpose.map(&:sum)
+    counts_format = format(texts, width)
+    counts_format << total_line_format
+  end
+
+  # sub methods
+  def store_counts(text)
+    counts = []
+    counts << text.lines if @show_lines
+    counts << text.words if @show_words
+    counts << text.bytesize if @show_bytesize
+    counts
+  end
+
+  def calc_total_of_each_counts(counts_list)
+    counts_list.transpose.map(&:sum)
+  end
+
+  def format_line_of_total(counts_list, width)
+    total_counts = calc_total_of_each_counts(counts_list)
     total_counts_format = rjust_counts_width(total_counts, width)
     total_line_format = total_counts_format << 'total'
-    total_line_format = total_line_format.join(' ')
-
-    counts_format << total_line_format
+    total_line_format.join(' ')
   end
 
   def rjust_counts_width(counts, width)
